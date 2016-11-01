3b785bf69d335d038989b7142a797cd58af4e2a0
Guillaume Bandet <guillaume.bandet@polytechnique.org>
2016-11-01 14:04:47 +0100

    Show win percentage.

--- site/smartfighter/front/views.py
+++ site/smartfighter/front/views.py
@@ -8,5 +8,5 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(TemplateView, self).get_context_data(**kwargs)
         context['games'] = Game.objects.all().select_related().order_by('-date')[:20]
-        context['ranking'] = Player.objects.all().order_by('-elo_rating')
+        context['ranking'] = Player.objects.prefetch_related('games_as_first_player', 'games_as_second_player').order_by('-elo_rating')
         return context
