4e3ebd4666aa609e56978d2e0afad74e6efa07c1
Guillaume Bandet <guillaume.bandet@polytechnique.org>
2016-11-05 10:52:24 +0100

    Does not show players with no played matches.

--- site/smartfighter/front/views.py
+++ site/smartfighter/front/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Q
 from django.views.generic import TemplateView
 
 from smartfighter.apps.ranking.models import Game, Player
@@ -8,5 +9,8 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(TemplateView, self).get_context_data(**kwargs)
         context['games'] = Game.objects.all().select_related().order_by('-date')[:20]
-        context['ranking'] = Player.objects.prefetch_related('games_as_first_player', 'games_as_second_player').order_by('-elo_rating')
+        context['ranking'] =  Player.objects.annotate(
+            first=Count('games_as_first_player')).annotate(second=Count('games_as_second_player')).filter(
+                Q(first__gt=0)|Q(second__gt=0)).prefetch_related(
+                    'games_as_first_player', 'games_as_second_player').order_by('-elo_rating')
         return context
