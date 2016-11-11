949bd694ed7e351088e4cd5234d0ababa8cbd8a2
Guillaume Bandet <guillaume.bandet@polytechnique.org>
2016-11-11 11:19:20 +0100

    Add an instructions page.

--- site/smartfighter/front/urls.py
+++ site/smartfighter/front/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from smartfighter.front.views import IndexView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^$', IndexView.as_view(), name='ranking'),
+    url(r'^instructions', TemplateView.as_view(template_name='front/instructions.html'),
+        name='instructions')
 ]
