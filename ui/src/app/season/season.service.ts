8209e6c7601adeda911fe2f04c1e1ad5704528e5
Guillaume Bandet <gbandet@multiposting.fr>
2017-08-11 08:36:28 +0200

    Fix for aot compilation.

--- ui/src/app/season/season.service.ts
+++ ui/src/app/season/season.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import { Game } from '../game/game';
 import { Page } from '../paging';
