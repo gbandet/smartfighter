7163f621e4a090922e2734ce32ac17b4c6a97eff
Guillaume Bandet <guillaume.bandet@polytechnique.org>
2017-11-05 17:57:16 +0100

    Remove trailing slashes in API URLs.

--- site/smartfighter/api/__init__.py
+++ site/smartfighter/api/__init__.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from smartfighter.api.views import GameViewSet, PlayerViewSet
 
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'game', GameViewSet)
 router.register(r'player', PlayerViewSet)
